36a9cc46ef911fd4aa45e136862a637d41b70a42
Chris Lewis <chrissam42@gmail.com>
2018-12-14 11:36:28 -0800

    working popup playground on specimens

--- varbro.js
+++ varbro.js
@@ -14,6 +14,7 @@ if (!window.doOnReady) {
 
 function varbroSetup() {
     overrideTNJS();
+    setupExamples();
     setupPlaygrounds();
 }
 
@@ -29,8 +30,96 @@ function overrideTNJS() {
     });
 }
 
+function doOverlay(content) {
+    var overlay = document.getElementById('playground-overlay');
+    if (overlay) {
+        overlay.parentElement.removeChild(overlay);
+    }
+    overlay = document.createElement('div');
+    overlay.id = 'playground-overlay';
+
+    var closeButton = document.createElement('button');
+    closeButton.textContent = "×";
+    closeButton.className = 'close';
+    closeButton.setAttribute('type', 'button');
+
+    if (typeof content === 'string') {
+        overlay.innerHTML = content;
+    } else {
+        overlay.appendChild(content);
+    }
+    
+    closeButton.addEventListener('click', function(evt) {
+        overlay.parentElement.removeChild(overlay);
+    });
+    
+    overlay.appendChild(closeButton);
+
+    document.body.appendChild(overlay);
+}
+
+function setupExamples() {
+/*
+<div class='playground'>
+    <div class='output frame'>
+        <div class='wrapper'></div>
+    </div>
+    <div class='html frame'>
+        <div class='editor' contenteditable></div>
+    </div>
+    <div class='css frame'>
+        <div class='editor' contenteditable></div>
+    </div>
+</div>
+*/
+    document.querySelectorAll('button.open-playground').forEach(function(button) {
+        var specimen = button.closest('.specimen');
+        button.addEventListener('click', function(evt) {
+            window.doAjax("/playground-template.html", {
+                'complete': function(xhr) {
+                    var temp = document.createElement('div');
+                    temp.innerHTML = xhr.responseText;
+                    var playground = temp.querySelector('.playground');
+                    var outputFrame = playground.querySelector('.output.frame .wrapper');
+                    var htmlFrame = playground.querySelector('.html.frame .editor');
+                    var cssFrame = playground.querySelector('.css.frame .editor');
+
+                    var styles = [];
+                    var codes = [];
+                    specimen.querySelectorAll('span.rendered').forEach(function(span) {
+                        var style = {};
+                        var subspec = span.closest('.specimen');
+                        style.tag = subspec.tagName.toLowerCase();
+                        style.className = subspec.className;
+                        style.css = span.getAttribute('style').split(/\s*;\s*/);
+                        styles.push(style);
+                        
+                        codes.push('<' + style.tag + ' class="' + subspec.className + '">\n  ' + span.innerHTML.trim() + '\n</' + style.tag + '>');
+                    });
+                    
+                    styles.forEach(function(style, i) {
+                        styles[i] = style.tag + '.' + style.className.trim().replace(/\s+/g, '.') + ' {\n  ' + style.css.join(";\n  ").replace(/:(\S)/g, ": $1").trim() + ';\n}';
+                    });
+                    
+                    cssFrame.textContent = styles.join("\n\n");
+
+                    htmlFrame.textContent = codes.join("\n\n");
+
+                    doOverlay(playground);
+                    
+                    setTimeout(setupPlaygrounds);
+                }
+            })
+        });
+    });
+}
+
 function setupPlaygrounds() { 
     document.querySelectorAll('.playground').forEach(function(playground, i) {
+        if (playground.getAttribute('data-processed') === 'true') {
+            return;
+        }
+
         var output = playground.querySelector('.output.frame');
         var html = playground.querySelector('.html.frame');
         var css = playground.querySelector('.css.frame');
@@ -64,6 +153,8 @@ function setupPlaygrounds() {
         });
         
         update();
+        
+        playground.setAttribute('data-processed', 'true');
     });
 }
 
